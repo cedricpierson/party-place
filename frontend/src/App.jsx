4a420cd64113d1f252f944c1186c54178a9c2765
Cédric PIERSON <cdric.pierson@gmail.com>
2022-10-26 16:45:52 +0200

    mobile button nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import MobileButtonNav from "./components/MobileButtonNav";
 
 function App() {
   return (
     <div className="App">
       <h1>PartyPlace</h1>
+      <MobileButtonNav />
     </div>
   );
 }
---
+++ frontend/src/components/MobileButtonNav.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import LocalDiningIcon from "@mui/icons-material/LocalDining";
+import LocalBarIcon from "@mui/icons-material/LocalBar";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import Paper from "@mui/material/Paper";
+
+function MobileButtonNav() {
+  return (
+    <Box sx={{ pb: 7 }}>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation showLabels value="" onChange="">
+          <BottomNavigationAction label="Recents" icon={<LocalDiningIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<LocalBarIcon />} />
+          <BottomNavigationAction label="Archive" icon={<LibraryMusicIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
+
+export default MobileButtonNav;

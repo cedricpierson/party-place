efb282a27cc026c7721fb9ba0f97148ceeb3c7df
Cédric PIERSON <cdric.pierson@gmail.com>
2022-10-27 13:56:35 +0200

    Add breakpoint mobile button nav + Landing,Home,Details,SpotifyPlayer empty components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import Details from "./components/Details";
 import ThemeProvider from "./theme";
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="App">
-        <h1>PartyPlace</h1>
-      </div>
+      <h1>PartyPlace</h1>
+      <Details />
     </ThemeProvider>
   );
 }

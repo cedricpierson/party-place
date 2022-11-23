bc75a7f2625ad37209c3a5addff9a08a60314420
Cédric PIERSON <cdric.pierson@gmail.com>
2022-11-23 09:12:31 +0100

    color header bg home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -49,7 +49,7 @@ export default function Home() {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "#D9C468",
+            backgroundColor: "#FFF5EC",
           }}
         >
           <Menuburger />
